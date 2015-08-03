b8bd42ec5cc2f18bf6874fdf98d5cf6e7b43aebd
Kiri Strack-Grose <kiristrackgrose@gmail.com>
2015-08-03 12:09:55 -0700

    solve engine problem in express server

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 /* dependencies */
 var express = require('express');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var http = require('http');
+var routes = require('./client/routes.js');
 
 var app = express();
 
-//route to angular index.html
+//middleware
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.use('./client', express.static('index'));
+//route to angular index.html
+// app.get('*', function(req, res) {
+//   res.render()
+// });
+
+app.listen(8080);
+
+// just in case
+module.exports = app;
